286943c40d70f5d13db863518f0c8194d6ade3b4
Lien Pham <lienpham56829@gmail.com>
2024-03-30 20:26:35 +0700

    tc checkbox

---
+++ cypress/e2e/exercises/element/checkbox.cy.ts
@@ -0,0 +1,20 @@
+describe('Test checkbox', () => {
+  beforeEach(() => {
+    cy.intercept('https://pagead2.googlesyndication.com/*', {})
+    cy.intercept('https://www.google-analytics.com/*', {})
+    cy.intercept('https://securepubads.g.doubleclick.net/*', {})
+    cy.intercept('https://stats.g.doubleclick.net/*', {})
+    cy.intercept('https://oajs.openx.net/*', {})
+    cy.intercept('https://id5-sync.com/*', {})
+    cy.visit('/checkbox')
+  })
+
+  it('select React checkbox only', () => {
+    cy.get('button.rct-collapse.rct-collapse-btn').click()
+    cy.get('button.rct-collapse.rct-collapse-btn').eq(2).click()
+    cy.get('button.rct-collapse.rct-collapse-btn').eq(3).click()
+    cy.get('input[type="checkbox"]').eq(4).click()
+  })
+})
+
+
