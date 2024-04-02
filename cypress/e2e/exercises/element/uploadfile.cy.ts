f7ace66926898818fd6e0cfd342cd866477c9e48
Lien Pham <lienpham56829@gmail.com>
2024-04-02 20:45:37 +0700

    test download file

--- cypress/e2e/exercises/element/uploadfile.cy.ts
+++ cypress/e2e/exercises/element/uploadfile.cy.ts
@@ -4,7 +4,13 @@ describe('Test upload & download', () => {
   })
 
   it('upload file', () => {
+    cy.get('#uploadFile').should('be.visible')
     cy.get('#uploadFile').selectFile('cypress/fixtures/may.png')
+    cy.get('#uploadedFilePath').should('contains.text', 'may.png')
   })
 
+  it('download file', () => {
+    cy.get('#downloadButton').should('be.visible').click()
+    cy.readFile('cypress/downloads/sampleFile.jpeg').should('exist')
+  });
 })
